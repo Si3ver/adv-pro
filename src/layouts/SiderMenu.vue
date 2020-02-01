21973bca8d0fdd00ca946d9748c29d40ec31d848
Si3ver <izhouwl@163.com>
2020-02-01 23:17:06 +0800

    根据用户点击, 把path添加进 $route.query, 并维护 selectedKeys 数组和 openKeys 数组

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -1,14 +1,12 @@
 <template>
   <div style="width: 256px">
-    <a-menu
-      :defaultSelectedKeys="selectedKeys"
-      :defaultOpenKeys="openKeys"
-      mode="inline"
-      :theme="theme"
-      :inlineCollapsed="collapsed"
-    >
+    <a-menu :selectedKeys="selectedKeys" :openKeys.sync="openKeys" mode="inline" :theme="theme">
       <template v-for="item in menuData">
-        <a-menu-item v-if="!item.children" :key="item.path">
+        <a-menu-item
+          v-if="!item.children"
+          :key="item.path"
+          @click="() => $router.push({ path: item.path, query: $route.query })"
+        >
           <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
           <span>{{ item.meta.title }}</span>
         </a-menu-item>
@@ -21,41 +19,56 @@
 <script>
 import SubMenu from "./SubMenu";
 export default {
-  components: {
-    "sub-menu": SubMenu
-  },
   props: {
     theme: {
       type: String,
       default: "dark"
     }
   },
+  components: {
+    "sub-menu": SubMenu
+  },
+  watch: {
+    "$route.path": function(val) {
+      this.selectedKeys = this.selectedKeysMap[val];
+      this.openKeys = this.collapsed ? [] : this.openKeysMap[val];
+    }
+  },
   data() {
+    this.selectedKeysMap = {};
+    this.openKeysMap = {};
     const menuData = this.getMenuData(this.$router.options.routes);
-    // console.log(JSON.stringify(menuData, null, 2));  // 左侧层级菜单
     return {
       collapsed: false,
       menuData,
-      selectedKeys: ["1"],
-      openKeys: ["2"]
+      selectedKeys: this.selectedKeysMap[this.$route.path],
+      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path]
     };
   },
   methods: {
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
     },
-    getMenuData(routes) {
+    getMenuData(routes = [], parentKeys = [], selectedKey) {
       const menuData = [];
       routes.forEach(item => {
         if (item.name && !item.hideInMenu) {
+          this.openKeysMap[item.path] = parentKeys;
+          this.selectedKeysMap[item.path] = [selectedKey || item.path];
           const newItem = { ...item };
           delete newItem.children;
           if (item.children && !item.hideChildrenInMenu) {
-            newItem.children = this.getMenuData(item.children);
+            newItem.children = this.getMenuData(item.children, [...parentKeys, item.path]);
+          } else {
+            this.getMenuData(
+              item.children,
+              selectedKey ? parentKeys : [...parentKeys, item.path],
+              selectedKey || item.path
+            );
           }
           menuData.push(newItem);
         } else if (!item.hideInMenu && !item.hideChildrenInMenu && item.children) {
-          menuData.push(...this.getMenuData(item.children));
+          menuData.push(...this.getMenuData(item.children, [...parentKeys, item.path]));
         }
       });
       return menuData;
@@ -63,5 +76,3 @@ export default {
   }
 };
 </script>
-
-<style></style>
